879ab9ba9ac9aeb636a26f4691ead12879aa3b42
Mohammed Dawood <mahmedtony73@gmail.com>
2022-09-27 23:12:59 +0200

    Exploring the Base Types

---
+++ basics.ts
@@ -0,0 +1,14 @@
+// Primitive: number | string | boolean | null | undefined | symbol
+// Object: any | void | never | unknown | bigint
+// Function types, parameters, and return types
+
+// Function types
+let age: number;
+age = 12;
+// age = "12"; // Error
+let userName: string;
+userName = "Mohammed";
+// userName = 12; // Error
+let isStudent: boolean;
+isStudent = true;
+// isStudent = 12; // Error
